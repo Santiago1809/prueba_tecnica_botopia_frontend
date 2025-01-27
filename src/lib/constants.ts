625c27f60f8d3f51e3e70c6b8dee301b21b52576
Santiago Aristizabal <aristizabalsantiago482@gmail.com>
2025-01-27 14:22:42 -0500

    Update backend host constants for development and production environments

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,11 +1,11 @@
 export const {
-  BACKEND_HOST = 'https://botopia-backend.onrender.com',
+  BACKEND_HOST = 'https://prueba-tecnica-botopia-backend.onrender.com',
   CLIENT_TOKEN,
-  NEXT_PUBLIC_BACKEND_HOST = 'https://botopia-backend.onrender.com',
+  NEXT_PUBLIC_BACKEND_HOST = 'https://prueba-tecnica-botopia-backend.onrender.com',
   NEXT_PUBLIC_PRUEBA,
   STRIPE_KEY,
   PAYPAL_CLIENT_ID,
   PAYPAL_SECRET,
-  HOST_LOCAL = "http://localhost:3000",
+  HOST_LOCAL = "https://botoshop.vercel.app",
   BACKEND_TOKEN_AI
 } = process.env;
